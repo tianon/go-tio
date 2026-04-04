1572debeca5899f4587f4f876d4546c95bfe2522
Tianon Gravi <admwiggin@gmail.com>
2026-04-04 02:08:03 -0700

    Make the tests even more heinous by nesting one inside another and double base64-ing

--- reader_test.go
+++ reader_test.go
@@ -13,27 +13,38 @@ func TestReaderAt(t *testing.T) {
 	for i := range len(b) {
 		b[i] = byte(i)
 	}
-	at := At{bytes.NewReader([]byte(base64.StdEncoding.EncodeToString(b)))}
+	b64 := []byte(base64.StdEncoding.EncodeToString(b))
+	b64b64 := []byte(base64.StdEncoding.EncodeToString(b64))
 	buf := make([]byte, len(b)+5)
-	for off := range len(b) + 1 { // +1 to make sure we test reading off the end
-		for size := range len(b) - off + 5 { // +5 to make sure we test reading way off the end at least one full base64 "chunk" or whatever it's called
-			exp := b[off:min(off+size, len(b))]
-			t.Run(fmt.Sprintf("%d+%d", off, size), func(t *testing.T) {
-				n, err := at.ReadAt(buf[:size], int64(off))
-				if err != nil && err != io.EOF {
-					t.Fatal(err)
-				} else if n != size && err != io.EOF {
-					t.Error("error should be io.EOF")
-				} else if n == size && off + size == len(b) && err != io.EOF {
-					t.Log("error could be io.EOF but isn't")
+	for _, td := range []struct {
+		name string
+		at   io.ReaderAt
+	}{
+		{name: "direct", at: At{bytes.NewReader(b64)}},
+		{name: "nested", at: At{At{bytes.NewReader(b64b64)}}},
+	} {
+		t.Run(td.name, func(t *testing.T) {
+			for off := range len(b) + 1 { // +1 to make sure we test reading off the end
+				for size := range len(b) - off + 5 { // +5 to make sure we test reading way off the end at least one full base64 "chunk" or whatever it's called
+					exp := b[off:min(off+size, len(b))]
+					t.Run(fmt.Sprintf("%d+%d", off, size), func(t *testing.T) {
+						n, err := td.at.ReadAt(buf[:size], int64(off))
+						if err != nil && err != io.EOF {
+							t.Fatal(err)
+						} else if n != size && err != io.EOF {
+							t.Error("error should be io.EOF")
+						} else if n == size && off+size == len(b) && err != io.EOF {
+							t.Log("error could be io.EOF but isn't")
+						}
+						if n != len(exp) {
+							t.Errorf("%d != %d", n, len(exp))
+						}
+						if !bytes.Equal(buf[:n], exp) {
+							t.Errorf("%v != %v", buf[:n], exp)
+						}
+					})
 				}
-				if n != len(exp) {
-					t.Errorf("%d != %d", n, len(exp))
-				}
-				if !bytes.Equal(buf[:n], exp) {
-					t.Errorf("%v != %v", buf[:n], exp)
-				}
-			})
-		}
+			}
+		})
 	}
 }
